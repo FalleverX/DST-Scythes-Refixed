edaae667587bea87a81c6e4dfd75c6a9c17ddb8b
Fallever <piscovip@163.com>
2023-04-17 01:14:22 +0800

    M 名字
    Signed-off-by: Fallever <piscovip@163.com>

--- modlangstrings.lua
+++ modlangstrings.lua
@@ -6,7 +6,7 @@ if locale == 'zh' or locale == 'zhr' or locale == 'zhr' then
     GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.SCYTHE = "砍倒成群的敌人!"
 
 
-    GLOBAL.STRINGS.NAMES.SCYTHE_GOLDEN = "金镰刀" --剽殷鎺菛灆
+    GLOBAL.STRINGS.NAMES.SCYTHE_GOLDEN = "黄金镰刀" --剽殷鎺菛灆
     GLOBAL.STRINGS.RECIPE_DESC.SCYTHE_GOLDEN = "更加有效的采集!"
     GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.SCYTHE_GOLDEN = "更加有效的采集!"
     GLOBAL.STRINGS.ACTIONS.MOWDOWN  = "收割"
